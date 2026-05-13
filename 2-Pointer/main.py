7b7a28c99fa04aed8b07be675eec9ee4738aa340
Aditya Kumar <aditya10112005@gmail.com>
2026-05-13 17:58:42 +0530

    Two Pointer

--- 2-Pointer/main.py
+++ 2-Pointer/main.py
@@ -54,3 +54,35 @@ def sorted_squares(nums):
         index -= 1
 
     return result
+
+# 44. Triplet Sum to Zero
+
+def three_sum(nums):
+    nums.sort()
+    triplets = []
+
+    for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue  # Skip duplicate elements
+
+        left, right = i + 1, len(nums) - 1
+
+        while left < right:
+            current_sum = nums[i] + nums[left] + nums[right]
+
+            if current_sum == 0:
+                triplets.append([nums[i], nums[left], nums[right]])
+                left += 1
+                right -= 1
+
+                # Skip duplicate elements
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+            elif current_sum < 0:
+                left += 1
+            else:
+                right -= 1
+
+    return triplets

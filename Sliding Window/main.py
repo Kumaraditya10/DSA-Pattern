ac0f53ca74a1a176c43cd47fe7035327e041c838
Aditya Kumar <aditya10112005@gmail.com>
2026-05-19 13:39:58 +0530

    Sliding Window

--- Sliding Window/main.py
+++ Sliding Window/main.py
@@ -1,16 +1,19 @@
 # 1. Maximum sum of a subarray of size k
 
 def max_sub_array_of_size_k(k, arr):
-    max_sum = 0
-    window_sum = 0
-    window_start = 0
+    low = 0
+    sum = 0
+    result = 0
 
-    for window_end in range(len(arr)):
-        window_sum += arr[window_end]
+    for high in range(len(arr)):
+        sum += arr[high]
 
-        if window_end >= k - 1:
-            max_sum = max(max_sum, window_sum)
-            window_sum -= arr[window_start]
-            window_start += 1
+        # window size becomes k
+        if high >= k - 1:
+            result = max(result, sum)
 
-    return max_sum
+            # slide window
+            sum -= arr[low]
+            low += 1
+
+    return result

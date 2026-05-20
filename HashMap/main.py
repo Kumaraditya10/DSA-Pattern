6e88e897aabba20fb5622cf0e527e4a14f1a3504
Aditya Kumar <aditya10112005@gmail.com>
2026-05-20 18:17:18 +0530

    HashMap

--- HashMap/main.py
+++ HashMap/main.py
@@ -11,3 +11,22 @@ def first_non_repeating_character(s):
             return i
 
     return -1
+
+
+# 2. Maximum number of Balloons
+
+def max_number_of_balloons(text):
+    freq = {}
+
+    for ch in text:
+        freq[ch] = freq.get(ch, 0) + 1
+
+    count = float('inf')
+
+    for ch in 'balloon':
+        count = min(count, freq.get(ch, 0))
+
+    # 'l' and 'o' are repeated twice in 'balloon'
+    count //= 2
+
+    return count 
